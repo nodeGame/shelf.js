1d0460616e4c942308e265b384d5b59cb06b2372
Stefano Balietti <futur.dorko@gmail.com>
2012-11-22 12:26:03 +0100

    bug in smoosh

--- bin/build.js
+++ bin/build.js
@@ -134,8 +134,9 @@ function buildIt(options) {
 	    }
 	    
 	    // builds both uncompressed and compressed files
-	    smooshed.build(); 
-	        
+	    //smooshed.build(); 
+	    smooshed.build('uncompressed'); 
+	    
     	if (options.analyse) {
     		smooshed.run(); // runs jshint on full build
     		smooshed.analyze(); // analyzes everything
