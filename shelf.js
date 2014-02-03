ad48438f45b9d3b4e87c50638126db92aee69704
Stefano Balietti <futur.dorko@gmail.com>
2014-02-03 11:41:26 +0100

    minor

--- shelf.js
+++ shelf.js
@@ -1,191 +1,189 @@
 /**
  * # Shelf.JS 
- * 
- * Persistent Client-Side Storage @VERSION
- * 
- * Copyright 2012 Stefano Balietti
+ * Copyright 2014 Stefano Balietti
  * GPL licenses.
+ *
+ * Persistent Client-Side Storage
  * 
  * ---
- * 
  */
 (function(exports){
-	
-var version = '0.3';
+    
+    var version = '0.5';
 
-var store = exports.store = function (key, value, options, type) {
+    var store = exports.store = function(key, value, options, type) {
 	options = options || {};
 	type = (options.type && options.type in store.types) ? options.type : store.type;
 	if (!type || !store.types[type]) {
-		store.log("Cannot save/load value. Invalid storage type selected: " + type, 'ERR');
-		return;
+	    store.log("Cannot save/load value. Invalid storage type selected: " + type, 'ERR');
+	    return;
 	}
 	store.log('Accessing ' + type + ' storage');
 	
 	return store.types[type](key, value, options);
-};
+    };
 
-// Adding functions and properties to store
-///////////////////////////////////////////
-store.name = "__shelf__";
+    // Adding functions and properties to store
+    ///////////////////////////////////////////
+    store.prefix = "__shelf__";
 
-store.verbosity = 0;
-store.types = {};
+    store.verbosity = 0;
+    store.types = {};
 
 
-var mainStorageType = "volatile";
+    var mainStorageType = "volatile";
 
-//if Object.defineProperty works...
-try {	
+    //if Object.defineProperty works...
+    try {	
 	
 	Object.defineProperty(store, 'type', {
-		set: function(type){
-			if ('undefined' === typeof store.types[type]) {
-				store.log('Cannot set store.type to an invalid type: ' + type);
-				return false;
-			}
-			mainStorageType = type;
-			return type;
-		},
-		get: function(){
-			return mainStorageType;
-		},
-		configurable: false,
-		enumerable: true
+	    set: function(type){
+		if ('undefined' === typeof store.types[type]) {
+		    store.log('Cannot set store.type to an invalid type: ' + type);
+		    return false;
+		}
+		mainStorageType = type;
+		return type;
+	    },
+	    get: function(){
+		return mainStorageType;
+	    },
+	    configurable: false,
+	    enumerable: true
 	});
-}
-catch(e) {
+    }
+    catch(e) {
 	store.type = mainStorageType; // default: memory
-}
+    }
 
-store.addType = function (type, storage) {
+    store.addType = function(type, storage) {
 	store.types[type] = storage;
-	store[type] = function (key, value, options) {
-		options = options || {};
-		options.type = type;
-		return store(key, value, options);
+	store[type] = function(key, value, options) {
+	    options = options || {};
+	    options.type = type;
+	    return store(key, value, options);
 	};
 	
 	if (!store.type || store.type === "volatile") {
-		store.type = type;
+	    store.type = type;
 	}
-};
+    };
 
-// TODO: create unit test
-store.onquotaerror = undefined;
-store.error = function() {	
+    // TODO: create unit test
+    store.onquotaerror = undefined;
+    store.error = function() {	
 	console.log("shelf quota exceeded"); 
 	if ('function' === typeof store.onquotaerror) {
-		store.onquotaerror(null);
+	    store.onquotaerror(null);
 	}
-};
+    };
 
-store.log = function(text) {
+    store.log = function(text) {
 	if (store.verbosity > 0) {
-		console.log('Shelf v.' + version + ': ' + text);
+	    console.log('Shelf v.' + version + ': ' + text);
 	}
 	
-};
+    };
 
-store.isPersistent = function() {
+    store.isPersistent = function() {
 	if (!store.types) return false;
 	if (store.type === "volatile") return false;
 	return true;
-};
+    };
 
-//if Object.defineProperty works...
-try {	
+    //if Object.defineProperty works...
+    try {	
 	Object.defineProperty(store, 'persistent', {
-		set: function(){},
-		get: store.isPersistent,
-		configurable: false
+	    set: function(){},
+	    get: store.isPersistent,
+	    configurable: false
 	});
-}
-catch(e) {
+    }
+    catch(e) {
 	// safe case
 	store.persistent = false;
-}
+    }
 
-store.decycle = function(o) {
+    store.decycle = function(o) {
 	if (JSON && JSON.decycle && 'function' === typeof JSON.decycle) {
-		o = JSON.decycle(o);
+	    o = JSON.decycle(o);
 	}
 	return o;
-};
+    };
     
-store.retrocycle = function(o) {
+    store.retrocycle = function(o) {
 	if (JSON && JSON.retrocycle && 'function' === typeof JSON.retrocycle) {
-		o = JSON.retrocycle(o);
+	    o = JSON.retrocycle(o);
 	}
 	return o;
-};
+    };
 
-store.stringify = function(o) {
+    store.stringify = function(o) {
 	if (!JSON || !JSON.stringify || 'function' !== typeof JSON.stringify) {
-		throw new Error('JSON.stringify not found. Received non-string value and could not serialize.');
+	    throw new Error('JSON.stringify not found. Received non-string value and could not serialize.');
 	}
 	
 	o = store.decycle(o);
 	return JSON.stringify(o);
-};
+    };
 
-store.parse = function(o) {
+    store.parse = function(o) {
 	if ('undefined' === typeof o) return undefined;
 	if (JSON && JSON.parse && 'function' === typeof JSON.parse) {
-		try {
-			o = JSON.parse(o);
-		}
-		catch (e) {
-			store.log('Error while parsing a value: ' + e, 'ERR');
-			store.log(o);
-		}
+	    try {
+		o = JSON.parse(o);
+	    }
+	    catch (e) {
+		store.log('Error while parsing a value: ' + e, 'ERR');
+		store.log(o);
+	    }
 	}
 	
 	o = store.retrocycle(o);
 	return o;
-};
+    };
 
-// ## In-memory storage
-// ### fallback for all browsers to enable the API even if we can't persist data
-(function() {
+    // ## In-memory storage
+    // ### fallback for all browsers to enable the API even if we can't persist data
+    (function() {
 	
 	var memory = {},
-		timeout = {};
+	timeout = {};
 	
 	function copy(obj) {
-		return store.parse(store.stringify(obj));
+	    return store.parse(store.stringify(obj));
 	}
 
 	store.addType("volatile", function(key, value, options) {
-		
-		if (!key) {
-			return copy(memory);
-		}
+	    
+	    if (!key) {
+		return copy(memory);
+	    }
 
-		if (value === undefined) {
-			return copy(memory[key]);
-		}
+	    if (value === undefined) {
+		return copy(memory[key]);
+	    }
 
-		if (timeout[key]) {
-			clearTimeout(timeout[key]);
-			delete timeout[key];
-		}
+	    if (timeout[key]) {
+		clearTimeout(timeout[key]);
+		delete timeout[key];
+	    }
 
-		if (value === null) {
-			delete memory[key];
-			return null;
-		}
+	    if (value === null) {
+		delete memory[key];
+		return null;
+	    }
 
-		memory[key] = value;
-		if (options.expires) {
-			timeout[key] = setTimeout(function() {
-				delete memory[key];
-				delete timeout[key];
-			}, options.expires);
-		}
+	    memory[key] = value;
+	    if (options.expires) {
+		timeout[key] = setTimeout(function() {
+		    delete memory[key];
+		    delete timeout[key];
+		}, options.expires);
+	    }
 
-		return value;
+	    return value;
 	});
-}());
+    }());
 
 }('undefined' !== typeof module && 'undefined' !== typeof module.exports ? module.exports: this));
